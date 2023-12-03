e3af85e046613cb95191b0c0b4d86cd1c5f71974
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2023-12-03 17:03:08 +0300

    Add project files.

---
+++ BlogWebApp.Entity/Entities/Article.cs
@@ -0,0 +1,35 @@
+﻿using BlogWebApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogWebApp.Entity.Entities
+{
+    public class Article : EntityBase ,IEntityBase
+    {
+
+
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+
+        public int ViewCount { get; set; }
+
+
+        public Guid CatagoryId { get; set; }
+        public Category Catagory { get; set; }
+
+        public Guid ImageId { get; set; }
+
+        public Image Image { get; set; }
+
+
+
+
+
+    }
+}
---
+++ BlogWebApp.Entity/Entities/Image.cs
@@ -0,0 +1,23 @@
+﻿using BlogWebApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogWebApp.Entity.Entities
+{
+    public class Image : EntityBase, IEntityBase
+    {
+     
+
+        public string Fileame { get; set; }
+
+        public string  FileType { get; set; }
+
+
+        public ICollection<Image> Images { get; set;}
+
+
+    }
+}
